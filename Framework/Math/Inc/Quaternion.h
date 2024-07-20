828f72ac1df57969589fbff45206417f3988e08e
JuukenNoHimeKasra <67715045+KasraKickii@users.noreply.github.com>
2024-07-20 06:13:32 -0700

    updates

--- Framework/Math/Inc/Quaternion.h
+++ Framework/Math/Inc/Quaternion.h
@@ -30,5 +30,23 @@ namespace Kick_Engine::Math
         // Constants
         static const Quaternion Identity;
         static const Quaternion Zero;
+        void Conjugate() noexcept;
+        void Inverse() noexcept;
+        float Magnitude() const noexcept;
+        float MagnitudeSqr() const noexcept;
+        void Normalize() noexcept;
+        float Dot(const Quaternion& q) const noexcept;
+
+        // Static Functions
+        static Quaternion Conjugate(const Quaternion& q);
+        static float Magnitude(const Quaternion& q);
+        static Quaternion Normalize(const Quaternion& q);
+
+        static Quaternion CreateFromAxisAngle(const Vector3& axis, float angle) noexcept;
+        static Quaternion CreateFromYawPitchRoll(float yaw, float pitch, float roll) noexcept;
+        static Quaternion CreateFromRotationMatrix(const Matrix4& m) noexcept;
+
+        static Quaternion Lerp(const Quaternion& q0, const Quaternion& q1, float t);
+        static Quaternion SLerp(const Quaternion& q0, const Quaternion& q1, float t);
     };
 }
